5f67010ae554749657fd458dd21f1689a859f51b
Ant767 <antspa767@gmail.com>
2024-07-14 03:09:49 -0500

    v2

--- chunkManager.js
+++ chunkManager.js
@@ -16,7 +16,7 @@ function generateSimpleChunk(chunkX, chunkZ) {
             let treeNoise = Math.abs(Perlin.simplex2((x + (chunkX * 16)), (z + (chunkZ * 16)))) * 256
             let value = Math.floor(Math.abs(Perlin.perlin2((x + (chunkX * 16)) / 100, (z + (chunkZ * 16)) / 100)) * 256);
             let threshold = 40;
-            let newY = value > threshold ? Math.floor((value / 512) * 25) + 50 : 50
+            let newY = value > 150 ? Math.floor((value / 512) * 60) + 50 : value > threshold ? Math.floor((value / 512) * 25) + 50 : 50
             chunk.setBlockType(new Vec3(x, newY, z), value <= threshold ? 26 : 8)
             if(value <= threshold) {
                 chunk.setBlockType(new Vec3(x, newY + 1, z), value <= threshold ? 26 : 8)
@@ -73,21 +73,39 @@ function generateSimpleChunk(chunkX, chunkZ) {
                 }
                 if(newY > 50) {
                     for(let i = 50; i < newY;i++) {
-                        chunk.setBlockType(new Vec3(x, i, z), 9)
+                        let off = Perlin.simplex2((x + (chunkX * 16)) / 40, (z + (chunkZ * 16)) / 40);
+                        chunk.setBlockType(new Vec3(x, i, z), i >= newY - (3 + Math.floor(off * 3)) ? 9 : 1)
                     }
                 }
             }
             for (let y = 0; y < 256; y++) {
-                if(y < 50) {
-                    // let val = Math.abs(Perlin.perlin3(Math.abs(x + (chunkX * 16) / 137.5), Math.abs(y), Math.abs(z + (chunkZ * 16) / 137.5))) * 256;
-                    // if(val > 10) {
-                        chunk.setBlockType(new Vec3(x, y, z), 1)
+                if(y == 1) {
+                    chunk.setBlockType(new Vec3(x, y, z), 27)
+                }
+                // if(value > 100 && y < 100 && y > 50) {
+                    // let val = Math.abs(Perlin.perlin3((x + (chunkX * 16)) / 100, Math.abs(y) / 100, (z + (chunkZ * 16))  / 10)) * 256;
+                    // if(val > (y / 10)) {
+                    //     chunk.setBlockType(new Vec3(x, y, z), 1)
     
                     // } else {
+
+                    // }
+                // }
+    
+                if(y < 50) {
+                    let val = Math.abs(Perlin.perlin3((x + (chunkX * 16)) / 137.5, Math.abs(y) / 137.5, (z + (chunkZ * 16))  / 137.5)) * 256;
+                    if(val > 10) {
+                        chunk.setBlockType(new Vec3(x, y, z), 1)
+    
+                    } else {
                     //     let choices = [2,4,6];
                     //     chunk.setBlockType(new Vec3(x, y, z), choices[Math.floor(Math.random() * choices.length)]);
-                    // }
+                    }
     
+                }
+                if(y == 0) {
+                    chunk.setBlockType(new Vec3(x, y, z), 25)
+
                 }
 
                 chunk.setSkyLight(new Vec3(x, y, z), 15)
@@ -103,13 +121,25 @@ class ChunkManager {
         this.world.on('blockUpdate', async (oldBlock, newBlock) => {
             if(oldBlock.material == "dirt" && newBlock.stateId == 0) {
                 let block = await this.world.getBlock(new Vec3(oldBlock.position.x, oldBlock.position.y + 1, oldBlock.position.z))
-                if(block.material == "plant") {
+                if(block.material == "plant" && block.type < 59 && block.type > 64) {
                     await this.world.setBlockStateId(block.position, 0)
                 }
             }
+            if((oldBlock.type == 161 || (oldBlock.type >= 485 && oldBlock.type <= 489)) && newBlock.stateId == 0) {
+                let block1 = await this.world.getBlock(new Vec3(oldBlock.position.x, oldBlock.position.y - 1, oldBlock.position.z))
+                let block2 = await this.world.getBlock(new Vec3(oldBlock.position.x, oldBlock.position.y + 1, oldBlock.position.z))
+                if(block1.type == 161 || (block1.type >= 485 && block1.type <= 489)) {
+                    await this.world.setBlockStateId(block1.position, 0)
+                }
+                if(block2.type == 161 || (block2.type >= 485 && block2.type <= 489)) {
+                    await this.world.setBlockStateId(block2.position, 0)
+                }
+            }
+            // 161 + 485,489
             if(oldBlock.stateId == 0 && newBlock.material == "plant") {
                 let block = await this.world.getBlock(new Vec3(oldBlock.position.x, oldBlock.position.y - 1, oldBlock.position.z))
-                if(block.transparent || block.material == "plant") {
+                console.log(block.material)
+                if(block.material == "plant" && block.type < 59 && block.type > 64) {
                     await this.world.setBlockStateId(newBlock.position, 0)
                 }
             }
--- main.js
+++ main.js
@@ -16,10 +16,35 @@ const broadcastPositionUpdates = require('./utils/broadcastPositionUpdates');
 const config = yaml.parse(fs.readFileSync('config.yaml').toString())
 const server = mc.createServer(config.server);
 let entityIdIndex = server.maxPlayers + 1;
+function sendBroadcastMessage(server, clients, message, sender) {
+    if (mcData.supportFeature('signedChat')) {
+        server.writeToClients(clients, 'player_chat', {
+            plainMessage: message,
+            signedChatContent: '',
+            unsignedChatContent: JSON.stringify({ text: message }),
+            type: 0,
+            senderUuid: 'd3527a0b-bc03-45d5-a878-2aafdd8c8a43', // random
+            senderName: JSON.stringify({ text: sender }),
+            senderTeam: undefined,
+            timestamp: Date.now(),
+            salt: 0n,
+            signature: mcData.supportFeature('useChatSessions') ? undefined : Buffer.alloc(0),
+            previousMessages: [],
+            filterType: 0,
+            networkName: JSON.stringify({ text: sender })
+        });
+    } else {
+        server.writeToClients(clients, 'chat', { message: JSON.stringify({ text: message }), position: 0, sender: sender || '0' });
+    }
+}
+
+function broadcast(message, exclude, username) {
+    sendBroadcastMessage(server, Object.values(server.clients).filter(client => client !== exclude), message);
+}
 
 function createEntityId() {
-  entityIdIndex++;
-  return entityIdIndex;
+    entityIdIndex++;
+    return entityIdIndex;
 }
 function offsetDir(x, y, z, dir) {
     if (dir == 1) return [x, y + 1, z]
@@ -29,8 +54,10 @@ function offsetDir(x, y, z, dir) {
     else if (dir == 5) return [x + 1, y, z]
     else if (dir == 6) return [x, y - 1, z]
 }
+let players = [];
 chunkManager.world.getColumn(0, 0)
-server.on('playerJoin', async (client)=>{
+server.on('playerJoin', async (client) => {
+    if(!players.includes(client.username)) players.push(client.username)
     fs.appendFileSync(`logs.txt`, `\n[${new Date().toString()}] ${client.username} Joined`)
     console.log(client.username + " joined!")
     client.id = createEntityId();
@@ -43,12 +70,39 @@ server.on('playerJoin', async (client)=>{
     }
     console.log(chunk.chunk.blockLightMask)
     // Object.values(server.clients).forEach(existingClient => {
-        // if (existingClient !== client) {
-            // spawnPlayer(client, existingClient);
-        // }
+    // if (existingClient !== client) {
+    // spawnPlayer(client, existingClient);
+    // }
     //   });
-    client.on('end', function () {
+    let blockUpdate = async (oldBlock, newBlock) => {
+        let chunkLoc = { x: Math.floor(oldBlock.position.x / 16), z: Math.floor(oldBlock.position.z / 16) };
+        let chunk = {
+            x: chunkLoc.x,
+            z: chunkLoc.z,
+            chunk: await chunkManager.world.getColumn(chunkLoc.x, chunkLoc.z)
+        }
+        client.write('map_chunk', {
+            x: chunk.x,
+            z: chunk.z,
+            // groundUp: chunksLoaded.find(_=>_[0]==chunk.x&&_[1]==chunk.z) ? false : true,
+            groundUp: true,
+            biomes: chunk.chunk.dumpBiomes !== undefined ? chunk.chunk.dumpBiomes() : undefined,
+            heightmaps: {
+                type: 'compound',
+                name: '',
+                value: {} // Client will accept fake heightmap
+            },
+            bitMap: chunk.chunk.getMask(),
+            chunkData: chunk.chunk.dump(),
+            blockEntities: []
+        });
 
+    }
+
+    client.on('end', function () {
+        chunkManager.world.off("blockUpdate", blockUpdate)
+        broadcast(`§e${client.username} left!`);
+        players = players.filter(_=>_ != client.username);
     });
     client.write('login', {
         ...mcData.loginPacket,
@@ -65,7 +119,8 @@ server.on('playerJoin', async (client)=>{
         enableRespawnScreen: true,
         isDebug: false,
         isFlat: false
-      });
+    });
+    broadcast(`§e${client.username} joined!`);
     // let skyLight = [], blockLight = [];
     // chunk.chunk.skyLightSections.forEach(e => e !== null && skyLight.push(new Uint8Array(e.data.buffer)));
     // chunk.chunk.blockLightSections.forEach(e => e !== null && blockLight.push(new Uint8Array(e.data.buffer)));
@@ -76,9 +131,9 @@ server.on('playerJoin', async (client)=>{
         groundUp: true,
         biomes: chunk.chunk.dumpBiomes !== undefined ? chunk.chunk.dumpBiomes() : undefined,
         heightmaps: {
-          type: 'compound',
-          name: '',
-          value: {} // Client will accept fake heightmap
+            type: 'compound',
+            name: '',
+            value: {} // Client will accept fake heightmap
         },
         bitMap: chunk.chunk.getMask(),
         chunkData: chunk.chunk.dump(),
@@ -96,9 +151,9 @@ server.on('playerJoin', async (client)=>{
     let chunksLoaded = [];
     async function getChunksNear() {
         let chunks = await chunkGen.getChunksNearLoc(client.position);
-        for(const chunk of chunks) {
-            if(chunksLoaded.find(_=>_[0]==chunk.x && _[1]==chunk.z)) continue;
-            chunksLoaded.push([chunk.x,chunk.z])
+        for (const chunk of chunks) {
+            if (chunksLoaded.find(_ => _[0] == chunk.x && _[1] == chunk.z)) continue;
+            chunksLoaded.push([chunk.x, chunk.z])
             client.write('map_chunk', {
                 x: chunk.x,
                 z: chunk.z,
@@ -106,24 +161,24 @@ server.on('playerJoin', async (client)=>{
                 groundUp: true,
                 biomes: chunk.chunk.dumpBiomes !== undefined ? chunk.chunk.dumpBiomes() : undefined,
                 heightmaps: {
-                  type: 'compound',
-                  name: '',
-                  value: {} // Client will accept fake heightmap
+                    type: 'compound',
+                    name: '',
+                    value: {} // Client will accept fake heightmap
                 },
                 bitMap: chunk.chunk.getMask(),
                 chunkData: chunk.chunk.dump(),
                 blockEntities: []
             });
-        
+
         }
-    
+
     }
-    let prevChunkLoc = {x:Math.floor(client.position.x / 16),z:Math.floor(client.position.z / 16)}
+    let prevChunkLoc = { x: Math.floor(client.position.x / 16), z: Math.floor(client.position.z / 16) }
     getChunksNear();
     client.on('position', function (data) {
         client.position = { x: data.x, y: data.y, z: data.z };
-        let newChunkLoc = {x:Math.floor(client.position.x / 16),z:Math.floor(client.position.z / 16)};
-        if(newChunkLoc.x != prevChunkLoc.x || newChunkLoc.z != prevChunkLoc.z) {
+        let newChunkLoc = { x: Math.floor(client.position.x / 16), z: Math.floor(client.position.z / 16) };
+        if (newChunkLoc.x != prevChunkLoc.x || newChunkLoc.z != prevChunkLoc.z) {
             getChunksNear()
             prevChunkLoc = newChunkLoc;
         }
@@ -131,123 +186,164 @@ server.on('playerJoin', async (client)=>{
     });
     let currSlot = 0;
     let playerSlots = {};
-    chunkManager.world.on('blockUpdate', async (oldBlock, newBlock) => {
-        let chunkLoc = {x:Math.floor(oldBlock.position.x / 16),z:Math.floor(oldBlock.position.z / 16)};
-        let chunk = {
-            x: chunkLoc.x,
-            z: chunkLoc.z,
-            chunk: await chunkManager.world.getColumn(chunkLoc.x, chunkLoc.z)
-          }
-          client.write('map_chunk', {
-            x: chunk.x,
-            z: chunk.z,
-            // groundUp: chunksLoaded.find(_=>_[0]==chunk.x&&_[1]==chunk.z) ? false : true,
-            groundUp: true,
-            biomes: chunk.chunk.dumpBiomes !== undefined ? chunk.chunk.dumpBiomes() : undefined,
-            heightmaps: {
-              type: 'compound',
-              name: '',
-              value: {} // Client will accept fake heightmap
-            },
-            bitMap: chunk.chunk.getMask(),
-            chunkData: chunk.chunk.dump(),
-            blockEntities: []
-        });
+    chunkManager.world.on('blockUpdate', blockUpdate);
+            // 161 + 485,489
 
-    });
     client.on('block_place', async function (packet) {
         try {
-            
+
             if (playerSlots[currSlot]) {
                 let itemName = mcData.items[playerSlots[currSlot]].name
-                if(!mcData.blocksByName[itemName]) return;
-        
+                if (!mcData.blocksByName[itemName]) return;
+
                 let off = offsetDir(packet.location.x, packet.location.y, packet.location.z, packet.direction)
                 let block2 = await chunkManager.world.getBlock(new Vec3(packet.location.x, packet.location.y, packet.location.z));
-                if(block2.material == "plant") {
-                    off = [packet.location.x,packet.location.y,packet.location.z]
+                if (block2.material == "plant" && block2.type < 59 && block2.type > 64) {
+                    off = [packet.location.x, packet.location.y, packet.location.z]
                 }
-                let chunkLoc = {
-                    x: Math.floor(off[0] / 16),
-                    z: Math.floor(off[2] / 16),
+                // rock wood wool leaves
+                let block3 = await chunkManager.world.getBlock(new Vec3(...off))
+                if(["rock","wood","wool","leaves"].includes(block3.material)) return;
+                if(
+                    !(
+                        (
+                            off[0] == Math.floor(client.position.x) &&
+                            off[1] == Math.floor(client.position.y) &&
+                            off[2] == Math.floor(client.position.z)    
+                        ) || (
+                            off[0] == Math.floor(client.position.x) &&
+                            off[1] == Math.floor(client.position.y) + 1 &&
+                            off[2] == Math.floor(client.position.z)    
+                        )
+    
+                    )
+                ) {
+                    let chunkLoc = {
+                        x: Math.floor(off[0] / 16),
+                        z: Math.floor(off[2] / 16),
+                    }
+                    console.log(chunkLoc)
+                    let id = mcData.blocksByName[itemName].id
+                    let block = Block.fromString(itemName);
+                    await chunkManager.world.setBlock(new Vec3(...off), block);
+                    let stateId = mcData.blocksByName[itemName].defaultState
+                    //35, 58
+                    console.log(block.type)
+
+            // 161 + 485,489
+                    if(block.type >= 35 && block.type <= 58) {
+                        stateId = packet.direction;
+                        await chunkManager.world.setBlockType(new Vec3(...off), block.type);
+                        console.log(stateId)
+                        await chunkManager.world.setBlockData(new Vec3(...off), packet.direction == 1 ? 1 : packet.direction == 2 ? 2 : packet.direction == 3 ? 2 : 0);
+                    } else if(block.type == 161 || (block.type >= 485 && block.type <= 489)) {
+                        await chunkManager.world.setBlockType(new Vec3(...off), block.type);
+                        await chunkManager.world.setBlockData(new Vec3(...off), 10);
+                        await chunkManager.world.setBlockType(new Vec3(off[0],off[1]+1,off[2]), block.type);
+                        await chunkManager.world.setBlockData(new Vec3(off[0],off[1]+1,off[2]), 2);
+
+                    } else {
+                        await chunkManager.world.setBlockStateId(new Vec3(...off), stateId);
+                    }
+                    let chunk = {
+                        x: chunkLoc.x,
+                        z: chunkLoc.z,
+                        chunk: await chunkManager.world.getColumn(chunkLoc.x, chunkLoc.z)
+                    }
+    
                 }
-                console.log(chunkLoc)
-                let id = mcData.blocksByName[itemName].id
-                let block = Block.fromString(itemName);
-                await chunkManager.world.setBlock(new Vec3(...off), block);
-                await chunkManager.world.setBlockStateId(new Vec3(...off), mcData.blocksByName[itemName].defaultState);
-                let chunk = {
-                    x: chunkLoc.x,
-                    z: chunkLoc.z,
-                    chunk: await chunkManager.world.getColumn(chunkLoc.x, chunkLoc.z)
-                  }
-                  server.writeToClients(Object.values(server.clients), 'map_chunk', {
+                server.writeToClients(Object.values(server.clients), 'map_chunk', {
                     x: chunk.x,
                     z: chunk.z,
                     groundUp: true,
                     biomes: chunk.chunk.dumpBiomes !== undefined ? chunk.chunk.dumpBiomes() : undefined,
                     heightmaps: {
-                      type: 'compound',
-                      name: '',
-                      value: {}
+                        type: 'compound',
+                        name: '',
+                        value: {}
                     },
                     bitMap: chunk.chunk.getMask(),
                     chunkData: chunk.chunk.dump(),
                     blockEntities: []
                 });
             }
-        } catch(e){console.error(e)}
+        } catch (e) { console.error(e) }
     });
-    client.on('chat',packet=>{
-        let sender = null;
-        server.writeToClients(server.clients, 'chat', { message: JSON.stringify({ text: `<${client.username}> ${packet.message}` }), position: 0, sender: sender || '0' });
+    client.on('chat', data => {
+        if(data.message == "/list") {
+            sendBroadcastMessage(server, [client], `§eThere are §b${players.length} player(s) §eonline: §r${players.join('§7, §r')}`);
+            return;
+        }
+        if(data.message.startsWith("/tp ")) {
+            try {
+                let args = data.message.split(' ').slice(1);
+                let arg1 = parseInt(args[0]);
+                let arg2 = parseInt(args[1]);
+                let arg3 = parseInt(args[2]);
+                client.write('position', {
+                    x: arg1,
+                    y: arg2,
+                    z: arg3,
+                    yaw: 0,
+                    pitch: 0,
+                    flags: 0x00
+                });
+            
+            } catch {}
+            return;
+        }
+        let adminUsernames = ["AzaleaDev","FruitKitty_"]
+        broadcast((adminUsernames.includes(client.username) ? "§b[BugMC Admin] §r" : "§f") + "<" + client.username + "> " + data.message, null, client.username);
+
+        // let sender = null;
+        // server.writeToClients(server.clients, 'chat', { message: JSON.stringify({ text: `<${client.username}> ${packet.message}` }), position: 0, sender: sender || '0' });
     })
     client.on('held_item_slot', (packet) => {
         currSlot = packet.slotId + 36;
-      })
-      client.on('set_creative_slot', (packet) => {
+    })
+    client.on('set_creative_slot', (packet) => {
         console.log(packet);
         console.log(packet.item)
         playerSlots[packet.slot] = packet.item.itemId;
-      })
-      client.on('block_dig', async function (packet) {
+    })
+    client.on('block_dig', async function (packet) {
         try {
-          let chunkLoc = {
-            x: Math.floor(packet.location.x / 16),
-            z: Math.floor(packet.location.z / 16),
-          }
-          await chunkManager.world.setBlock(new Vec3(packet.location.x, packet.location.y, packet.location.z), Block.fromString('minecraft:air'))
-          let chunk = {
-            x: chunkLoc.x,
-            z: chunkLoc.z,
-            chunk: await chunkManager.world.getColumn(chunkLoc.x, chunkLoc.z)
-          }
-          server.writeToClients(Object.values(server.clients), 'map_chunk', {
-            x: chunk.x,
-            z: chunk.z,
-            groundUp: true,
-            biomes: chunk.chunk.dumpBiomes !== undefined ? chunk.chunk.dumpBiomes() : undefined,
-            heightmaps: {
-              type: 'compound',
-              name: '',
-              value: {} // Client will accept fake heightmap
-            },
-            bitMap: chunk.chunk.getMask(),
-            chunkData: chunk.chunk.dump(),
-            blockEntities: []
-        });
-    
+            let chunkLoc = {
+                x: Math.floor(packet.location.x / 16),
+                z: Math.floor(packet.location.z / 16),
+            }
+            await chunkManager.world.setBlock(new Vec3(packet.location.x, packet.location.y, packet.location.z), packet.location.y < 53 ? Block.fromString('minecraft:water') : Block.fromString('minecraft:air'))
+            let chunk = {
+                x: chunkLoc.x,
+                z: chunkLoc.z,
+                chunk: await chunkManager.world.getColumn(chunkLoc.x, chunkLoc.z)
+            }
+            server.writeToClients(Object.values(server.clients), 'map_chunk', {
+                x: chunk.x,
+                z: chunk.z,
+                groundUp: true,
+                biomes: chunk.chunk.dumpBiomes !== undefined ? chunk.chunk.dumpBiomes() : undefined,
+                heightmaps: {
+                    type: 'compound',
+                    name: '',
+                    value: {} // Client will accept fake heightmap
+                },
+                bitMap: chunk.chunk.getMask(),
+                chunkData: chunk.chunk.dump(),
+                blockEntities: []
+            });
+
         } catch { }
-    
-      });
+
+    });
     // setInterval(()=>{
     //     getChunksNear()
     // },1000);
 })
 server.on('error', function (error) {
     console.log('Error:', error);
-  });
-  
-  server.on('listening', function () {
+});
+
+server.on('listening', function () {
     console.log('Server listening on port', server.socketServer.address().port);
-  });
+});
